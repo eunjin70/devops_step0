d55fce81856707522ecc630969b2dcb1b6a1c1e8
eunjin70 <leeunzin@gmail.com>
2024-09-29 11:46:26 +0900

    test code update 후 Action확인

--- src/test/java/kr/ac/devops/devopts_step0/DevoptsStep0ApplicationTests.java
+++ src/test/java/kr/ac/devops/devopts_step0/DevoptsStep0ApplicationTests.java
@@ -1,5 +1,7 @@
 package kr.ac.devops.devopts_step0;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,4 +12,19 @@ class DevoptsStep0ApplicationTests {
 	void contextLoads() {
 	}
 
+	int add(int i, int j){
+		return i+j;
+	}
+	//gradle build할때 테스트 확인하자
+	@Test
+	void addTest1(){
+		int result =add(10,20);
+		assertEquals(30, result);
+	}
+
+	@Test
+	void addTest2(){
+		int result =add(10,20);
+		assertEquals(20, result);
+	}
 }
